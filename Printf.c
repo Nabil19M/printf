46b3a1dfc97f15dfc3d2dad03dcd9c649553f69a
Nabil19M <115401006+Nabil19M@users.noreply.github.com>
2023-12-05 20:29:03 +0200

    Create Printf.c

---
+++ Printf.c
@@ -0,0 +1,48 @@
+#include "main.h"
+/**
+* _printf - produces output according to a format.
+* @format: The specified format.
+* Return: The number of characters that were printed
+*/
+int _printf(const char *format, ...)
+{
+        va_list args;
+        int count = 0;
+        int (*fun)(va_list);
+
+        va_start(args, format);
+        if (format == NULL)
+        return (-1);
+        while (*format)
+        {
+                if (*format == '%')
+                {
+                        format++;
+                        if (!*format)
+                        return (-1);
+                        if (*format == '%')
+                        {
+                                _putchar('%');
+                                count++;
+                        }
+                        else
+                        {
+                          fun = get_fun((char *)format);
+                                if (fun != NULL)
+                                        count += fun(args);
+                                else
+                                {
+                                        _putchar('%');
+                                        _putchar(*format);
+                                        count += 2;
+                                }}}
+                else
+                {
+                        _putchar(*format);
+                        count++;
+                }
+                format++;
+        }
+        va_end(args);
+        return (count);
+}

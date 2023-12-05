007824aaa102fab4ce949138b293779011d95531
Nabil19M <115401006+Nabil19M@users.noreply.github.com>
2023-12-05 19:55:23 +0200

    Create main.h

---
+++ main.h
@@ -0,0 +1,26 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+#include <stdarg.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <limits.h>
+/**
+ * struct format - Struct format
+ *
+ * @sp:char pointer of the specifier
+ * @f: pointer to function for the conversion specifier
+ */
+typedef struct specifier
+{
+        char *sp;
+        int (*f)(va_list);
+} spec_form;
+
+int _printf(const char *format, ...);
+int char_fun(va_list args);
+int int_fun(va_list args);
+int str_fun(va_list args);
+int (*get_fun(char *format))(va_list);
+int _putchar(char c);
+
+#endif

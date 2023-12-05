817f32f24e1c4b25c4fdb5eb45d8360d18a2094f
Abdulla Omar <aabdula2712@gmail.com>
2023-12-05 21:51:09 +0200

    file 1

--- Printf.c
+++ Printf.c
@@ -6,43 +6,45 @@
 */
 int _printf(const char *format, ...)
 {
-        va_list args;
-        int count = 0;
-        int (*fun)(va_list);
+	va_list args;
+	int count = 0;
+	int (*fun)(va_list);
 
-        va_start(args, format);
-        if (format == NULL)
-        return (-1);
-        while (*format)
-        {
-                if (*format == '%')
-                {
-                        format++;
-                        if (!*format)
-                        return (-1);
-                        if (*format == '%')
-                        {
-                                _putchar('%');
-                                count++;
-                        }
-                        else
-                        {
-                          fun = get_fun((char *)format);
-                                if (fun != NULL)
-                                        count += fun(args);
-                                else
-                                {
-                                        _putchar('%');
-                                        _putchar(*format);
-                                        count += 2;
-                                }}}
-                else
-                {
-                        _putchar(*format);
-                        count++;
-                }
-                format++;
-        }
-        va_end(args);
-        return (count);
+	va_start(args, format);
+	if (format == NULL)
+	return (-1);
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++;
+			if (!*format)
+			return (-1);
+			if (*format == '%')
+			{
+				_putchar('%');
+				count++;
+			}
+			else
+			{
+				fun = get_fun((char *)format);
+				if (fun != NULL)
+				count += fun(args);
+				else
+				{
+					_putchar('%');
+					_putchar(*format);
+					count += 2;
+				}
+			}
+		}
+		else
+		{
+			_putchar(*format);
+			count++;
+		}
+		format++;
+	}
+	va_end(args);
+	return (count);
 }

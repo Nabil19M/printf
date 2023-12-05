c7652abd261881d9dbca7e61fa32ce7c707d3d64
Nabil19M <115401006+Nabil19M@users.noreply.github.com>
2023-12-05 20:30:34 +0200

    Create str_fun.c

---
+++ str_fun.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdarg.h>
+
+/**
+* str_fun - produces output according to the format %s
+* @args: The arguments
+* Return: 0 on success
+*/
+int str_fun(va_list args)
+{
+        char *str = va_arg(args, char*);
+        int len = 0;
+
+        if (str == NULL)
+        {
+                str = "(null)";
+        }
+        while (str[len] != '\0')
+        {
+                _putchar(str[len]);
+                len++;
+        }
+        return (len);
+}

645dbd63cec99e67897ab46b19dcfc3be8eeb7c9
root <nabelmohamed154004@gmail.com>
2023-12-05 11:33:36 -0800

    last bit

---
+++ int_fun.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdarg.h>
+#include <math.h>
+/**
+* int_fun - produces output according to a format %d or %i
+* @args: The arguments
+* Return: 0 on success
+*/
+
+int int_fun(va_list args)
+{
+	int n = va_arg(args, int);
+	int count = 0;
+	unsigned int num;
+	int d;
+
+	if (n == 0)
+	{
+		_putchar('0');
+		return (1);
+	}
+	if (n < 0)
+	{
+		_putchar('-');
+		count++;
+		num = n * -1;
+	}
+	else
+		num = n;
+	while(num / d > 9)
+		d*=10;
+	while(d != 0)
+	{
+		count += _putchar(num / d + '0');
+		num %= d;
+		d /= 10;
+	}
+	return (count);
+}

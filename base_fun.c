7841594be68ba8ba3518993672958aa2adb3c7de
Nabil19M <115401006+Nabil19M@users.noreply.github.com>
2023-12-07 17:48:32 +0200

    Update base_fun.c

--- base_fun.c
+++ base_fun.c
@@ -9,7 +9,7 @@ int bin_fun(va_list args)
 {
 	unsigned int num = va_arg(args, unsigned int);
 	char bin_string[33] = {0};
-	int len = 1;
+	int len = 0;
 	int i;
 
 	if (num == 0)
@@ -23,9 +23,7 @@ int bin_fun(va_list args)
 		num >>= 1;
 	}
 		bin_string[0] = '\0';
-	if (bin_string == NULL)
-		return (-1);
-	for (i = len; i >= 0; i--)
+	for (i = len - 1; i >= 0; i--)
 		_putchar(bin_string[i]);
 	return (len);
 }
